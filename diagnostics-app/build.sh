370bfa2782629554de71de9dda51d206cef589a3
Ethan Cheng <yycheng@google.com>
2023-08-21 09:18:06 +0000

    [IWA] Define build script for generating PWA + IWA
    Define a build script, which will:
    1. build the PWA via `ng build`.
    2. build the IWA via npm CLI commands
    
    The build script will copy the distribution code in `/dist/pwa` into a
    intermediate folder, copy the `/iwa/public` files into the same
    intermediate folder, then package it into a web bundle.
    
    BUG: b/295127885

---
+++ diagnostics-app/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Build the angular project
+ng build
+
+# Create folders
+mkdir -p dist/iwa
+iwa_src=$(mktemp -d)
+web_bundle=$(mktemp)
+
+# Copy the files into the temporary folders
+cp -r dist/diagnostics-app/* ${iwa_src}
+cp -r iwa/public/* ${iwa_src}
+
+# Create the bundle
+wbn --dir ${iwa_src} --output ${web_bundle} --formatVersion b2
+wbn-sign -i ${web_bundle} -o dist/iwa/sample-iwa.swbn -k ed25519key.pem
+
+# Delete the resources
+rm -r ${iwa_src}
+rm ${web_bundle}
