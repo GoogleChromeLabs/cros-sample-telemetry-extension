d569edc7155b893f75c540f4ce2424f860256f5d
Jessica Hsiao <djhsiao@google.com>
2023-08-28 07:43:03 +0000

    [UI] Create Events module and dashboard component
    Events module and Events dashboard component are created.
    
    BUG: b/297807810

---
+++ diagnostics-app/src/app/events/events-dashboard/events-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-dashboard',
+  templateUrl: './events-dashboard.component.html',
+  styleUrls: ['./events-dashboard.component.css']
+})
+export class EventsDashboardComponent {
+
+}
