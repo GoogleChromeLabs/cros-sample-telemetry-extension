16d174908d787b94096f7ab28e754cfdc1d88fb9
Jessica Hsiao <djhsiao@google.com>
2023-09-07 10:50:17 +0000

    [UI] Implement TelemetryDashboard Component
    An array is declared in file telemetry-dashboard.component.ts to specify
    which telemetry types should be displayed in the dashboard. And the
    program loops through those types and uses telemetry service to request
    and receive the desired data.
    
    And the TelemetryCardComponent is used in the template for showing
    telemetry base cards.
    
    BUG: b/295126796

--- diagnostics-app/src/app/telemetry/telemetry-dashboard/telemetry-dashboard.component.ts
+++ diagnostics-app/src/app/telemetry/telemetry-dashboard/telemetry-dashboard.component.ts
@@ -3,20 +3,29 @@
 // found in the LICENSE file.
 
 /**
- * @fileoverview Defines the Dashboard Component.
- * This is the root layout component which holds the dashboard.
- * Imported by dashboard.module.ts
+ * @fileoverview Defines the Dashboard Component in Telemetry.
+ * This is the root layout component which holds the telemetry dashboard.
+ * Imported by telemetry.module.ts
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TelemetryInfoType } from '@common/message';
 
 @Component({
   selector: 'app-telemetry-dashboard',
   templateUrl: './telemetry-dashboard.component.html',
   styleUrls: ['./telemetry-dashboard.component.css'],
 })
-export class TelemetryDashboardComponent implements OnInit {
-  constructor() {}
+export class TelemetryDashboardComponent {
+  // Array of telemetry info types that will be displayed
+  private _visibleTypes: TelemetryInfoType[] = [
+    TelemetryInfoType.BATTERY,
+    TelemetryInfoType.BLOCK_DEVICE,
+    TelemetryInfoType.CPU,
+    TelemetryInfoType.MEMORY,
+    TelemetryInfoType.STATEFUL_PARTITION,
+    TelemetryInfoType.VPD,
+  ];
 
-  ngOnInit(): void {}
+  get visibleTypes() { return this._visibleTypes; }
 }
