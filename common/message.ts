ab6b3b0a2b1a12f863994608d3f260b20d3db1ed
Jessica Hsiao <djhsiao@google.com>
2023-09-13 02:19:57 +0000

    Update Diagnostics type and message definitions
    Since the current definitions in dpsl.ts are outdated, they are changed
    to align with the definitions in the chromium's code base. And the
    definitions in message.ts are also modified.
    
    BUG: b/295126759

--- common/message.ts
+++ common/message.ts
@@ -7,7 +7,12 @@
  * diagostics-app and diagnostics-extension
  */
 
-import { DpslTypes, DiagnosticsParams, RoutineStatus } from './dpsl';
+import {
+  DpslTypes,
+  DiagnosticsParams,
+  GetRoutineUpdateResponse,
+  RoutineType,
+} from './dpsl';
 
 export const enum RequestType {
   TELEMETRY = 'telemetry',
@@ -24,26 +29,24 @@ export const enum TelemetryInfoType {
   VPD = 'vpd',
 }
 
-export const enum ResponseErrorInfoMessage {
+export const enum ResponseErrorInfoMessage { // not sure if each word should be connected with '-'
   InvalidRequestType = 'Invalid or missing request type.',
   InvalidTelemetryInfoType = 'The requested telemetry infoType is either invalid or missing.',
+  InvalidDiagnosticsRoutineStatus = 'The diagnostics routine status is invalid.',
   MissingTelemetryRequest = 'Missing telemetry object in request.',
   MissingDiagnosticsRequest = 'Missing diagnostics object in request.',
   InvalidDiagnosticsAction = 'The requested diagnostics action is either invalid or missing.',
+  InvalidDiagnosticsParams = 'The requested diagnostics params are either invalid or missing.',  
   InvalidDiagnosticsRoutineName = 'The requested diagnostics routine name is either invalid or missing.',
   InvalidDiagnosticsRoutineId = 'The requested diagnostics routine id is either invalid or missing.',
   UnsupportedTelemetryFunction = 'Unsupported operation. The requested telemetry function is not supported',
 }
 
-export const enum DiagnosticsRoutineName {
-  RUN_BATTERY_CAPACITY_ROUTINE = 'runBatteryCapacityRoutine',
-}
-
 export const enum DiagnosticsAction {
   START = 'start',
   STATUS = 'status',
-  RESUME = 'resume',
   STOP = 'stop',
+  RESUME = 'resume',
 }
 
 export interface TelemetryRequest {
@@ -53,7 +56,7 @@ export interface TelemetryRequest {
 export interface DiagnosticsRequest {
   action: DiagnosticsAction;
   routineId?: number;
-  routineName?: DiagnosticsRoutineName;
+  routineName?: RoutineType;
   params?: DiagnosticsParams;
 }
 
@@ -72,8 +75,8 @@ export interface TelemetryResponse {
 }
 
 export interface DiagnosticsResponse {
-  routineId: number;
-  routineStatus: RoutineStatus;
+  id: number;
+  info: GetRoutineUpdateResponse;
 }
 
 export interface Response {
