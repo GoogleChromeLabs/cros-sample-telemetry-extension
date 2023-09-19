8fccb4671d57aaa61fd38bde80198859e234ad27
Jessica Hsiao <djhsiao@google.com>
2023-09-19 03:02:19 +0000

    [UI] Change data refresh interval format
    Since there will be refresh intervals for fetching telemetry info,
    diagnostics routine update, and events update, the refreshIntervals is
    changed from a map that can be accessed using telemetry info types to an
    object that has a key 'telemetry' to a value with type
    Map<string, number> that uses telemetry info type as key and refresh
    interval as value.
    
    And a variable defaultTelemetryRefreshInterval is set as a default value
    of refresh interval.
    
    BUG: b/295126796

--- diagnostics-app/src/app/core/config/data-refresh-intervals.ts
+++ diagnostics-app/src/app/core/config/data-refresh-intervals.ts
@@ -1,5 +1,11 @@
-export const refreshIntervals = new Map<string, number> ([
-    ['memory', 1000],
-    ['storage', 60000],
-    ['cpu', 1000],
-])
+import { TelemetryInfoType } from '@common/message';
+
+export const defaultTelemetryRefreshInterval = 10000;
+
+export const refreshIntervals = {
+    telemetry: new Map<string, number> ([
+        [TelemetryInfoType.BATTERY, 1000],
+        [TelemetryInfoType.CPU, 1000],
+        [TelemetryInfoType.STATEFUL_PARTITION, 60000]
+    ])
+}
