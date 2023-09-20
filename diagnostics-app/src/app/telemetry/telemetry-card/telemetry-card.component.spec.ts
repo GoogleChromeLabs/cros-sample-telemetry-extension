9cc91322d82e2f36523181103e7b16a28ef1b1d2
Jessica Hsiao <djhsiao@google.com>
2023-09-20 11:03:35 +0000

    [UI] Create TelemetryCard Component
    The TelemetryCardComponent is created, and it takes one input: type,
    which specifies which type of telemetry info it is fetching.
    
    As for the content of the card, when it is fetching the data, a loading
    progress spinner will be shown, and when there is an error, a yellow
    error message will be displayed, and when the data is successfully
    aquired, it renders TelemetryCardContentComponent and sends the data as
    input.
    
    BUG: b/295126796

---
+++ diagnostics-app/src/app/telemetry/telemetry-card/telemetry-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TelemetryCardComponent } from './telemetry-card.component';
+import { TelemetryInfoType } from '@common/message';
+
+describe('TelemetryCardComponent', () => {
+  let component: TelemetryCardComponent;
+  let fixture: ComponentFixture<TelemetryCardComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TelemetryCardComponent]
+    });
+    fixture = TestBed.createComponent(TelemetryCardComponent);
+    component = fixture.componentInstance;
+    component.type = TelemetryInfoType.BATTERY;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- diagnostics-app/src/app/telemetry/telemetry.module.ts
+++ diagnostics-app/src/app/telemetry/telemetry.module.ts
@@ -10,11 +10,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { TelemetryCardComponent } from './telemetry-card/telemetry-card.component';
 import { TelemetryCardContentComponent } from './telemetry-card/telemetry-card-content/telemetry-card-content.component';
 import { TelemetryDashboardComponent } from './telemetry-dashboard/telemetry-dashboard.component';
 
 @NgModule({
   declarations: [
+    TelemetryCardComponent,
     TelemetryCardContentComponent,
     TelemetryDashboardComponent
   ],
