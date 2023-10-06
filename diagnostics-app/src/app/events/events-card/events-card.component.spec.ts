7b139b17c6222d8e5f51c898284f6fbf0b82f4ba
Jessica Hsiao <djhsiao@google.com>
2023-10-06 10:52:48 +0000

    [UI] Create EventsCardComponent
    BUG: b/295127175

---
+++ diagnostics-app/src/app/events/events-card/events-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventCategory } from '@common/dpsl';
+import { EventsCardComponent } from './events-card.component';
+import { EventsService } from 'src/app/core/services/events.service';
+
+describe('EventsCardComponent', () => {
+  let component: EventsCardComponent;
+  let fixture: ComponentFixture<EventsCardComponent>;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [ EventsCardComponent ],
+      providers: [ EventsService ]
+    });
+    fixture = TestBed.createComponent(EventsCardComponent);
+    component = fixture.componentInstance;
+    component.category = EventCategory.audio_jack;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- diagnostics-app/src/app/events/events.module.ts
+++ diagnostics-app/src/app/events/events.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EventsCardComponent } from './events-card/events-card.component';
 import { EventsCardContentComponent } from './events-card/events-card-content/events-card-content.component';
 import { EventsDashboardComponent } from './events-dashboard/events-dashboard.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
+    EventsCardComponent,
     EventsCardContentComponent,
     EventsDashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class EventsModule { }
