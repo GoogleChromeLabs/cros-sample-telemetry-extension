1c3b0e1b75a942539b5f6f577ea613c41a0a9590
Jessica Hsiao <djhsiao@google.com>
2023-10-06 10:51:43 +0000

    [UI] Create EventsCardContentComponent
    BUG: b/295127175

--- diagnostics-app/src/app/events/events.module.ts
+++ diagnostics-app/src/app/events/events.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { EventsCardContentComponent } from './events-card/events-card-content/events-card-content.component';
 import { EventsDashboardComponent } from './events-dashboard/events-dashboard.component';
-
-
 
 @NgModule({
   declarations: [
+    EventsCardContentComponent,
     EventsDashboardComponent
   ],
   imports: [
