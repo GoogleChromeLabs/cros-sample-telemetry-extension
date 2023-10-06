f57cfd3144037b60795d88349b5213368b505947
Jessica Hsiao <djhsiao@google.com>
2023-10-06 10:51:43 +0000

    [UI] Create EventsCardContentComponent
    BUG: b/295127175

---
+++ diagnostics-app/src/app/events/events-card/events-card-content/events-card-content.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventsCardContentComponent } from './events-card-content.component';
+
+describe('EventsCardContentComponent', () => {
+  let component: EventsCardContentComponent;
+  let fixture: ComponentFixture<EventsCardContentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [EventsCardContentComponent]
+    });
+    fixture = TestBed.createComponent(EventsCardContentComponent);
+    component = fixture.componentInstance;
+    component.event = {};
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
