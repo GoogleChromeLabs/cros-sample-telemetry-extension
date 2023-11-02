ea4aa4cc734a849c2d4aa09e3388e70159974fe6
Ethan Cheng <yycheng@google.com>
2023-11-02 07:52:54 +0000

    [Build] Add testing keys and change extension IDs
    Add the public testing keys used in testing.
    
    Change environment extension ID to match extension produced by the
    testing key.
    
    BUG: NONE

--- diagnostics-app/src/environments/environment.iwa.ts
+++ diagnostics-app/src/environments/environment.iwa.ts
@@ -4,6 +4,6 @@
 
 export const environment = {
   production: true,
-  extensionId: 'gogonhoemckpdpadfnjnpgbjpbjnodgc',
+  extensionId: 'jmalcmbicpnakfkncbgbcmlmgpfkhdca',
   serviceWorker: false
 };
