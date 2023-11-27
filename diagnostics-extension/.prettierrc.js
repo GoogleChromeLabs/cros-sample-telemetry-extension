53775c38bc62eaa380b85450e12e7f1080a3bc63
Ethan Cheng <yycheng@google.com>
2023-11-27 06:01:49 +0000

    [Build] Add format rule and scripts
    Add the formatter rules and scripts to invoke the formatter.
    
    Prettier with custom config is used in the project.
    
    BUG=b/308944438

---
+++ diagnostics-extension/.prettierrc.js
@@ -0,0 +1,38 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const shared = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'preserve',
+  bracketSpacing: false,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  embeddedLanguageFormatting: 'off',
+  htmlWhitespaceSensitivity: 'strict',
+};
+
+const config = {
+  overrides: [
+    {
+      files: '*.ts',
+      options: shared,
+    },
+    {
+      files: '*.js',
+      options: shared,
+    },
+    {
+      files: '*.html',
+      options: {
+        printWidth: 100,
+      },
+    },
+  ],
+};
+
+module.exports = config;
