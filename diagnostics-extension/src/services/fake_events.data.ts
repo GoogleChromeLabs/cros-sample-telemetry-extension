e5ca889915f94324b96a9950324d319d4a0d66fc
Ethan Cheng <yycheng@google.com>
2023-11-30 05:58:51 +0000

    [Extension] Add framework for fake event data
    Add framework for fake event data. Currently all events will respond
    with "no mocked data", but this should suffice to test out UI change.
    
    BUG=b:313542363

--- diagnostics-extension/src/services/fake_events.data.ts
+++ diagnostics-extension/src/services/fake_events.data.ts
@@ -9,28 +9,81 @@
 // Fake data may not use certain parameter variables.
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import {EventCategory, EventSupportStatusInfo} from '@common/dpsl';
-import {Response} from '@common/message';
+import {
+  EventCategory,
+  EventSupportStatusInfo,
+  EventSupportStatus,
+} from '@common/dpsl';
+import {PortName, Response} from '@common/message';
+
+let eventPort: chrome.runtime.Port;
+const categoryToIntervalId: Map<
+  EventCategory,
+  ReturnType<typeof setTimeout>
+> = new Map<EventCategory, ReturnType<typeof setTimeout>>();
+const fakeEventData: Map<EventCategory, object> = new Map<
+  EventCategory,
+  object
+>();
+const fakeDataInterval: number = 3000; // Send fake data every 3 seconds.
 
 export const registerEventHandlers = async (): Promise<Response> => {
   return {success: true};
 };
+
 export const registerPort = (port: chrome.runtime.Port): void => {
+  eventPort = port;
   return;
 };
+
+const notifyPort = (type: EventCategory, message: object): void => {
+  if (!eventPort) {
+    return;
+  }
+  eventPort.postMessage({
+    type: type,
+    info: message,
+  });
+  return;
+};
+
+const getFakeData = (type: EventCategory): object => {
+  if (fakeEventData.has(type)) {
+    return fakeEventData.get(type)!;
+  }
+  return {type: 'no mocked data'};
+};
+
+// Default to support all events in fake data.
 export const isEventSupported = async (
   eventType: EventCategory,
 ): Promise<EventSupportStatusInfo> => {
-  const res: EventSupportStatusInfo = {};
+  const res: EventSupportStatusInfo = {
+    status: EventSupportStatus.supported,
+  };
   return res;
 };
+
+// Set a timer to periodically send fake data once event starts capturing.
 export const startCapturingEvents = async (
   eventType: EventCategory,
 ): Promise<void> => {
+  categoryToIntervalId.set(
+    eventType,
+    setInterval(() => {
+      notifyPort(eventType, getFakeData(eventType));
+    }, fakeDataInterval),
+  );
   return;
 };
+
+// Destroy the event timer.
 export const stopCapturingEvents = async (
   eventType: EventCategory,
 ): Promise<void> => {
+  if (categoryToIntervalId.has(eventType)) {
+    clearInterval(categoryToIntervalId.get(eventType));
+  }
+  categoryToIntervalId.delete(eventType);
   return;
 };
