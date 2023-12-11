24150be3d6b7ada983c77995368c15404c5d3828
Ethan Cheng <yycheng@google.com>
2023-12-11 08:03:50 +0000

    [App] Implement routine v2 page
    Modify the dashboard to show the routine v2 cards.
    
    BUG: b/315749078

--- diagnostics-app/src/app/routine-v2/routine-v2-dashboard/routine-v2-dashboard.component.ts
+++ diagnostics-app/src/app/routine-v2/routine-v2-dashboard/routine-v2-dashboard.component.ts
@@ -1,8 +1,54 @@
-import {Component} from '@angular/core';
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Defines the CardContent Component in Routine V2.
+ * Imported by routine-v2.module.ts
+ */
+
+import {Component, OnInit} from '@angular/core';
+import {RoutineV2Argument} from 'common/message';
+
+import {RoutineV2Service} from 'app/core/services/routine-v2.service';
+import {VISIBLE_ROUTINE_V2_CARDS} from 'common/config';
+import {RoutineSupportStatus} from 'common/telemetry-extension-types';
 
 @Component({
   selector: 'app-routine-v2-dashboard',
   templateUrl: './routine-v2-dashboard.component.html',
   styleUrls: ['./routine-v2-dashboard.component.css'],
 })
-export class RoutineV2DashboardComponent {}
+export class RoutineV2DashboardComponent implements OnInit {
+  private _error?: string; // the error message if there is any error
+
+  // Array of routine-v2 arguments that will actually be displayed
+  private _supportedRoutines: RoutineV2Argument[] = [];
+
+  get error() {
+    return this._error;
+  }
+  get supportedRoutines() {
+    return this._supportedRoutines;
+  }
+
+  async isRoutineArgumentSupported(argument: RoutineV2Argument) {
+    try {
+      const supportStatusInfo =
+        await this.RoutineV2Service.isRoutineArgumentSupported(argument);
+      if (supportStatusInfo.status === RoutineSupportStatus.supported) {
+        this._supportedRoutines.push(argument);
+      }
+    } catch (err) {
+      this._error = String(err);
+    }
+  }
+
+  constructor(private RoutineV2Service: RoutineV2Service) {}
+
+  ngOnInit(): void {
+    for (const argument of VISIBLE_ROUTINE_V2_CARDS) {
+      this.isRoutineArgumentSupported(argument);
+    }
+  }
+}
