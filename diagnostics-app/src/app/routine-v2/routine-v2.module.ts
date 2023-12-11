f174167ce5324cf3107cc2a59688e735de9465fd
Ethan Cheng <yycheng@google.com>
2023-12-11 08:02:58 +0000

    [App] Implement routine v2 card
    Implement the routine v2 card.
    
    BUG: b/315749078

--- diagnostics-app/src/app/routine-v2/routine-v2.module.ts
+++ diagnostics-app/src/app/routine-v2/routine-v2.module.ts
@@ -3,10 +3,15 @@ import {NgModule} from '@angular/core';
 
 import {SharedModule} from '../shared/shared.module';
 import {RoutineV2CardContentComponent} from './routine-v2-card/routine-v2-card-content/routine-v2-card-content.component';
+import {RoutineV2CardComponent} from './routine-v2-card/routine-v2-card.component';
 import {RoutineV2DashboardComponent} from './routine-v2-dashboard/routine-v2-dashboard.component';
 
 @NgModule({
-  declarations: [RoutineV2DashboardComponent, RoutineV2CardContentComponent],
+  declarations: [
+    RoutineV2DashboardComponent,
+    RoutineV2CardContentComponent,
+    RoutineV2CardComponent,
+  ],
   imports: [CommonModule, SharedModule],
 })
 export class RoutineV2Module {}
