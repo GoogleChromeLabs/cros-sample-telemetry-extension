789ad40ef6457272598575e2692b662035a7e966
Ethan Cheng <yycheng@google.com>
2024-03-21 07:56:54 +0000

    [RMA] Create base test component
    Create a base test component which outlines the necessary interface
    for a test component. Future tests (Routine v1, v2 or any custom test)
    should inherit this component.

---
+++ diagnostics-app/src/app/rma/test-viewer/base-test/base-test.component.ts
@@ -0,0 +1,17 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {TestConfig} from 'common/config/rma';
+
+// This component outlines the interface for a test component. All V1, V2 and
+// custom tests should inherit from this component.
+@Component({
+  selector: 'app-base-test',
+  templateUrl: './base-test.component.html',
+})
+export abstract class BaseTestComponent {
+  @Input() index!: number;
+  @Input() title!: string;
+  @Input() argument!: TestConfig['testArgument'];
+  @Output() testCompleted = new EventEmitter<void>();
+
+  abstract startRoutine(): void;
+}
