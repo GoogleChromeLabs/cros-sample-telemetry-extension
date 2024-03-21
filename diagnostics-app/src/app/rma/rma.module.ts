6e192244b0759b5e1d820d321ff19347a77ddd44
Ethan Cheng <yycheng@google.com>
2024-03-21 08:50:09 +0000

    [RMA] Create audit page component
    Create an audit page component. This component should be able to
    display aggregated information after running multiple tests.
    
    The UI can be seen here:
    https://screenshot.googleplex.com/7V6fKDqpkLgH6Ab

--- diagnostics-app/src/app/rma/rma.module.ts
+++ diagnostics-app/src/app/rma/rma.module.ts
@@ -6,6 +6,7 @@ import {ContentLayoutComponent} from './layout/content-layout/content-layout.com
 import {LogViewerComponent} from './log-viewer/log-viewer.component';
 import {RmaRoutingModule} from './rma-routing.module';
 import {RmaSidebarComponent} from './rma-sidebar/rma-sidebar.component';
+import {AuditPageComponent} from './test-viewer/audit-page/audit-page.component';
 import {JsonViewerComponent} from './test-viewer/json-viewer/json-viewer.component';
 import {RoutineV1TestComponent} from './test-viewer/routine-v1-test/routine-v1-test.component';
 import {RoutineV2TestComponent} from './test-viewer/routine-v2-test/routine-v2-test.component';
@@ -18,6 +19,7 @@ import {RoutineV2TestComponent} from './test-viewer/routine-v2-test/routine-v2-t
     JsonViewerComponent,
     RoutineV2TestComponent,
     RoutineV1TestComponent,
+    AuditPageComponent,
   ],
   imports: [RmaRoutingModule, SharedModule],
   exports: [ContentLayoutComponent],
---
+++ diagnostics-app/src/app/rma/test-viewer/audit-page/audit-page.component.ts
@@ -0,0 +1,74 @@
+import {Component, NgZone} from '@angular/core';
+import {
+  TestOrchestratorService,
+  TestResult,
+} from 'app/core/services/test-orchestrator.service';
+import {TestConfig} from 'common/config/rma';
+import {BaseTestComponent} from '../base-test/base-test.component';
+
+class TestStatistics {
+  enabledTests: number[] = [];
+  passedTests: number[] = [];
+  failedTests: number[] = [];
+  unranTests: number[] = [];
+  disabledTests: number[] = [];
+}
+
+@Component({
+  selector: 'app-audit-page',
+  templateUrl: './audit-page.component.html',
+  styleUrls: ['./audit-page.component.css'],
+})
+export class AuditPageComponent extends BaseTestComponent {
+  private testList: TestConfig[] = [];
+  private testResults: TestResult[] = [];
+  testStatistics = new TestStatistics();
+
+  UpdateTestStatistics() {
+    this.ngZone.run(() => {
+      this.testStatistics = new TestStatistics();
+      for (const [index, testConfig] of this.testList.entries()) {
+        if (!testConfig.enabled) {
+          this.testStatistics.disabledTests.push(index);
+          continue;
+        }
+        this.testStatistics.enabledTests.push(index);
+        const testInfo = this.testResults[index];
+        if (!testInfo) {
+          this.testStatistics.unranTests.push(index);
+        }
+        if (testInfo && !testInfo.passed) {
+          this.testStatistics.failedTests.push(index);
+        }
+        if (testInfo && testInfo.passed) {
+          this.testStatistics.passedTests.push(index);
+        }
+      }
+    });
+  }
+
+  public constructor(
+    private testOrchestrator: TestOrchestratorService,
+    private ngZone: NgZone,
+  ) {
+    super();
+  }
+
+  ngOnInit() {
+    this.testOrchestrator.getTestList$().subscribe((testList: TestConfig[]) => {
+      this.testList = testList;
+      this.UpdateTestStatistics();
+    });
+    this.testOrchestrator
+      .getTestResults$()
+      .subscribe((testResults: TestResult[]) => {
+        this.testResults = testResults;
+        this.UpdateTestStatistics();
+      });
+  }
+
+  // For compatibility with the base test component interface.
+  startRoutine(): void {
+    return;
+  }
+}
